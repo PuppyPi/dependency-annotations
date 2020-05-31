9163c54c4ca346d8c0ecc82b08c132186d532708
PuppyPi <puppyofcodez@gmail.com>
2020-05-31 02:21:49 +0000

    I've found this to be quite useful in semantizing resource files :)

---
+++ src/rebound/io/OpenableResource.java
@@ -0,0 +1,48 @@
+package rebound.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import rebound.annotations.semantic.meta.dependencies.DependencyFile;
+
+/**
+ * This is useful in many cases for rewriting code to encapsulate resource pathnames into something much more useful and portable :)
+ * 
+ * You declare public static final {@link OpenableResource}'s instead of {@link String}'s, then use {@link OpenableResource#open()} instead of {@link Class#getResourceAsStream(String)} or {@link ClassLoader#getResourceAsStream(String)} in everything that accepts those constants :)
+ * And in the classes that declare the constants, you simply attach a {@link DependencyFile} as appropriate!  And packagers/analyzers/etc. tools can recognize its resource-dependencies just like they recognize its class dependencies!  ^_^
+ * 
+ * If a resource name is really needed (say for error documentation or autodetermining the file type from the extension!), let the {@link #toString()} of this be called/implemented as in {@link #from(Class, String)}  :)
+ */
+@FunctionalInterface
+public interface OpenableResource
+{
+	/**
+	 * @return null on failure not an {@link IOException}, just like {@link Class#getResourceAsStream(String)}
+	 */
+	public InputStream open();
+	
+	
+	
+	
+	/**
+	 * Anything using someclass.getClassLoader().getResourceAsStream(...) can just use this with <code>someclass</code> and a forward-slash in front of the resource path  :3
+	 * @param c Class for context (since Java can support multiple instances of the same class in different sibling ClassLoaders and different resource-lookup paths for different classes among other complexities)
+	 * @param p The resource path, relative to the class of absolute if it begins with a /
+	 */
+	public static OpenableResource from(Class c, String p)
+	{
+		return new OpenableResource()
+		{
+			@Override
+			public InputStream open()
+			{
+				return c.getResourceAsStream(p);
+			}
+			
+			@Override
+			public String toString()
+			{
+				return p.startsWith("/") ? p : '/'+c.getPackage().getName().replace('.', '/')+'/'+p;
+			}
+		};
+	}
+}
--- src/rebound/util/uid/UID.java
+++ src/rebound/util/uid/UID.java
@@ -2,6 +2,7 @@ package rebound.util.uid;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.util.UUID;
 
 
 //Not documented :p
@@ -14,7 +15,10 @@ import java.lang.annotation.RetentionPolicy;
 public @interface UID
 {
 	/**
-	 * This is to be a hexadecimal string in either upper or lower case, and with optional hyphens, spaces and/or colons as delimiters.
+	 * If you only want to use UUIDs, just parse this value with {@link UUID#fromString(String)} XD
+	 * But if you want to support code using other syntaxes, read below.
+	 * 
+	 * Generally speaking, this is to be a hexadecimal string in either upper or lower case, and with optional hyphens, spaces and/or colons as delimiters.
 	 * The only rule about the contents is that it can't be the same as any other UID!  XD
 	 * (the UUID standard is the usual way to come up with them, but that's only one way :3 )
 	 * (and thus if they're UUIDs, they'll be interpreted as bytes (pairs of hex chars) as if the colons weren't there)
